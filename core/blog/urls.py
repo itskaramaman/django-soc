740c78accc8a38b6d80a9d87f951945b294e91f9
Karamjeet Singh <singhkaramjeetapple@gmail.com>
2024-01-14 14:28:41 -0800

    blog list and detail views created

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="blog-home"),
-    path('<int:id>/', views.blog, name="blog"),
+    path('', views.BlogListView.as_view(), name="blog-home"),
+    path('<int:pk>/', views.BlogDetailView.as_view(), name="blog"),
     path('about/', views.about, name="blog-about")
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,14 +1,30 @@
-from django.shortcuts import render, HttpResponse
+from typing import Any
+from django.shortcuts import render
 from .models import Blog
+from django.views.generic import ListView, DetailView
 
 
 def home(request):
     blogs = Blog.objects.all()
     return render(request, 'blog/home.html', {'title': 'Hello Django', 'blogs': blogs})
 
+
+class BlogListView(ListView):
+    model = Blog
+    template_name = "blog/home.html"  # <app>/<model>_<viewtype>.html
+    context_object_name = 'blogs'
+    ordering = ['-created_at']
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+    template_name = 'blog/blog.html'
+
+
 def blog(request, id):
     blog = Blog.objects.filter(id=id)
     return render(request, 'blog/home.html', {'blogs': blog})
+
 
 def about(request):
     return render(request, 'blog/about.html')
