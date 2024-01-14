6e982e50da2eacee6223d208a15d23f9ad839f74
Karamjeet Singh <singhkaramjeetapple@gmail.com>
2024-01-14 13:16:36 -0800

    signals added for user profile, user profile updates created

--- core/users/views.py
+++ core/users/views.py
@@ -1,15 +1,14 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .forms import UserRegisterForm
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            # form.save()
+            form.save()
             messages.success(
                 request,
                 'Your account has been created! You are now able to login'
@@ -22,5 +21,26 @@ def register(request):
 
 @login_required
 def profile(request):
-    print(request.user.profile.image.url)
-    return render(request, "users/profile.html")
+    if request.method == "POST":
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(
+            request.POST,
+            request.FILES,
+            instance=request.user.profile
+        )
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, "Your profile has been updated")
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        "user_form": user_form,
+        "profile_form": profile_form
+    }
+
+    return render(request, "users/profile.html", context)
