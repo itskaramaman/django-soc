21b86a288dbdc2adbcb6a1926ce896238af05d03
Karamjeet Singh <singhkaramjeetapple@gmail.com>
2024-01-18 12:38:48 -0800

    debug mode removed, readme updated

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # django-soc
+
+To run the project, use the following command:
+```bash
+docker-compose up --build
+
+The app will be accessible at:
+http://localhost:8000/
+```
--- core/core/settings.py
+++ core/core/settings.py
@@ -29,7 +29,6 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv('DEBUG', False))
-DEBUG = True
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", '127.0.0.1').split(" ")
 
